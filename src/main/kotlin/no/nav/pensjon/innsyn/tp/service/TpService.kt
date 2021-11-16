b39e3afaff5f1cb90f3b3128ceec94eb22d2f0fb
Mathias Sand Jahren <Mathias.Sand.Jahren@nav.no>
2021-11-16 13:44:56 +0100

    Log errors from TP.

--- src/main/kotlin/no/nav/pensjon/innsyn/tp/service/TpService.kt
+++ src/main/kotlin/no/nav/pensjon/innsyn/tp/service/TpService.kt
@@ -1,6 +1,7 @@
 package no.nav.pensjon.innsyn.tp.service
 
 import no.nav.pensjon.innsyn.tp.domain.Forhold
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono
 
 @Service
 class TpService(@Value("\${tp.url}") tpURL: String) {
+    private val log = LoggerFactory.getLogger(javaClass)
     private val webClient = WebClient.create(tpURL)
 
     fun getData(fnr: String, auth: String): List<Forhold> = webClient.get()
@@ -20,12 +22,10 @@ class TpService(@Value("\${tp.url}") tpURL: String) {
         }
         .retrieve()
         .onStatus({ !it.is2xxSuccessful }) {
-            Mono.error(
-                ResponseStatusException(
-                    HttpStatus.BAD_GATEWAY,
-                    "Error fetching data from TP. Status code: ${it.statusCode()}"
-                )
-            )
+            "Error fetching data from TP. Status code: ${it.statusCode()}".let { err ->
+                log.error(err)
+                Mono.error(ResponseStatusException(HttpStatus.BAD_GATEWAY, err))
+            }
         }
         .bodyToFlux<Forhold>()
         .collectList().block()!!
