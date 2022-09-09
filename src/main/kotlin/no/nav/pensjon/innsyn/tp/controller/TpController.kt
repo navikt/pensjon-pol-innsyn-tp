9735d569d2882920cb4a3d1391cd9778f676b723
TheMather1 <the.mather1@gmail.com>
2022-09-09 10:52:38 +0200

    Setup logback configuration.

--- src/main/kotlin/no/nav/pensjon/innsyn/tp/controller/TpController.kt
+++ src/main/kotlin/no/nav/pensjon/innsyn/tp/controller/TpController.kt
@@ -1,11 +1,14 @@
 package no.nav.pensjon.innsyn.tp.controller
 
+import io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE
 import no.nav.pensjon.innsyn.tp.CONTENT_TYPE_EXCEL
 import no.nav.pensjon.innsyn.tp.service.AzureTokenService
 import no.nav.pensjon.innsyn.tp.service.TpService
 import no.nav.pensjon.innsyn.tp.service.TpSheetProducer
 import org.apache.poi.xssf.streaming.SXSSFWorkbook
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory.getLogger
+import org.springframework.http.HttpHeaders.CONNECTION
 import org.springframework.http.HttpHeaders.CONTENT_DISPOSITION
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient
@@ -22,7 +25,7 @@ class TpController(
     private val tpService: TpService,
     private val azureTokenService: AzureTokenService
 ) {
-    val log = getLogger(javaClass)
+    val log: Logger = getLogger(javaClass)
 
     @GetMapping
     fun getTpInnsyn(
@@ -30,18 +33,15 @@ class TpController(
         @RegisteredOAuth2AuthorizedClient("azure") authorizedClient: OAuth2AuthorizedClient,
         response: HttpServletResponse
     ) {
-        log.info("Fetching data for ${fnr.substring(0, 5) + "*****"}")
+        log.info("Fetching data for $fnr.")
         val forhold = tpService.getData(fnr, azureTokenService.getOnBehalOfToken(authorizedClient))
-        response.apply {
-            addHeader("Content-Description", "File Transfer")
-            addHeader(CONTENT_DISPOSITION, "attachment; filename=$fnr.xlsx")
-            addHeader("Content-Transfer-Encoding", "binary")
-            addHeader("Connection", "Keep-Alive")
-            contentType = CONTENT_TYPE_EXCEL
-            SXSSFWorkbook(
-                worksheetProducer.produceWorksheet(forhold)
-            ).write(outputStream)
-        }
+        val worksheet = SXSSFWorkbook(worksheetProducer.produceWorksheet(forhold))
         log.info("Worksheet produced.")
+        response.apply {
+            addHeader(CONTENT_DISPOSITION, "attachment; filename=\"$fnr - TP.xlsx\"")
+            addHeader(CONNECTION, KEEP_ALIVE.toString())
+            contentType = CONTENT_TYPE_EXCEL
+            worksheet.write(outputStream)
+        }
     }
 }
