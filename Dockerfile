d7be75bab5ecabd4c36b9ffe797cd3fa3bd86001
TheMather1 <the.mather1@gmail.com>
2022-09-13 11:15:54 +0200

    Bump versions.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM navikt/java:17
+FROM navikt/java:18
 
 USER root
 RUN apt-get update && apt-get install -y \
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "no.nav.pensjon"
 version = "1"
 
-val jacksonVersion = "2.13.1"
+val jacksonVersion = "2.13.4"
 
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
-    id("org.springframework.boot") version "2.6.3"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
+    id("org.springframework.boot") version "2.7.3"
+    id("io.spring.dependency-management") version "1.0.13.RELEASE"
 }
 
 repositories {
@@ -20,22 +20,20 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310", jacksonVersion)
     implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", jacksonVersion)
-    implementation("io.micrometer", "micrometer-registry-prometheus", "1.8.3")
-    implementation("net.logstash.logback", "logstash-logback-encoder", "7.0.1")
+    implementation("io.micrometer", "micrometer-registry-prometheus", "1.9.3")
+    implementation("net.logstash.logback", "logstash-logback-encoder", "7.2")
     implementation("no.nav.common", "token-client", "2.2022.09.02_11.04-2530dd139a0a")
-    implementation("org.apache.poi", "poi-ooxml", "5.2.0")
+    implementation("org.apache.poi", "poi-ooxml", "5.2.2")
     implementation("org.springframework.boot", "spring-boot-starter-web")
     implementation("org.springframework.boot", "spring-boot-starter-webflux")
     implementation("org.springframework.boot", "spring-boot-starter-actuator")
     implementation("org.springframework.boot", "spring-boot-starter-validation")
     implementation("org.springframework.boot", "spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot", "spring-boot-starter-thymeleaf")
-    implementation("org.springframework.cloud", "spring-cloud-starter-contract-stub-runner", "3.1.0")
     testImplementation(kotlin("test-junit5"))
-    testImplementation("com.github.tomakehurst", "wiremock-jre8", "2.31.0")
-    testImplementation("com.ninja-squad", "springmockk", "3.1.0")
+    testImplementation("com.github.tomakehurst", "wiremock-jre8", "2.33.2")
+    testImplementation("com.ninja-squad", "springmockk", "3.1.1")
     testImplementation("org.springframework.boot", "spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
         exclude(module = "mockito-core")
     }
     testImplementation("org.springframework.security", "spring-security-test")
@@ -43,7 +41,7 @@ dependencies {
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
+        kotlinOptions.jvmTarget = "18"
     }
     test {
         useJUnitPlatform()
