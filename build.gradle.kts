e45ccdba19dd5aa9858a4c9bbf5afb7f0a5ff094
TheMather1 <the.mather1@gmail.com>
2023-11-29 14:48:23 +0100

    Bump versions.
    Co-authored-by: Pål Fleischer Børjeson <paalfb@yahoo.se>
    Co-authored-by: Martin Knutsen <martin.knutsen@nav.no>
    Co-authored-by: Jens Chr. Madsen <Jens.Christian.Madsen@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "no.nav.pensjon"
 version = "1"
 
-val jacksonVersion = "2.15.2"
+val jacksonVersion = "2.16.0"
 
 plugins {
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.spring") version "1.9.0"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
     id("org.springframework.boot") version "3.1.2"
-    id("io.spring.dependency-management") version "1.1.3"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 repositories {
@@ -20,10 +20,10 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310", jacksonVersion)
     implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", jacksonVersion)
-    implementation("io.micrometer", "micrometer-registry-prometheus", "1.9.3")
-    implementation("net.logstash.logback", "logstash-logback-encoder", "7.2")
+    implementation("io.micrometer", "micrometer-registry-prometheus", "1.12.0")
+    implementation("net.logstash.logback", "logstash-logback-encoder", "7.4")
     implementation("no.nav.common", "token-client", "2.2022.09.02_11.04-2530dd139a0a")
-    implementation("org.apache.poi", "poi-ooxml", "5.2.2")
+    implementation("org.apache.poi", "poi-ooxml", "5.2.5")
     implementation("org.springframework.boot", "spring-boot-starter-web")
     implementation("org.springframework.boot", "spring-boot-starter-webflux")
     implementation("org.springframework.boot", "spring-boot-starter-actuator")
@@ -31,8 +31,9 @@ dependencies {
     implementation("org.springframework.boot", "spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot", "spring-boot-starter-thymeleaf")
     testImplementation(kotlin("test-junit5"))
-    testImplementation("com.github.tomakehurst", "wiremock", "3.0.0-beta-10")
-    testImplementation("com.ninja-squad", "springmockk", "3.1.1")
+    testRuntimeOnly("org.junit.platform", "junit-platform-launcher")
+    testImplementation("com.github.tomakehurst", "wiremock", "3.0.1")
+    testImplementation("com.ninja-squad", "springmockk", "4.0.2")
     testImplementation("org.springframework.boot", "spring-boot-starter-test") {
         exclude(module = "mockito-core")
     }
@@ -41,7 +42,7 @@ dependencies {
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "18"
+        kotlinOptions.jvmTarget = "20"
     }
     test {
         useJUnitPlatform()
