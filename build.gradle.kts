1c47c3926fa14625681b18fe6a887eaa56e4fb47
Pål Fleischer Børjeson <paalfb@yahoo.se>
2025-03-31 16:32:36 +0200

    PES-631. Bump dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ version = "1"
 val jacksonVersion = "2.17.2"
 
 plugins {
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.spring") version "2.0.21"
-    id("org.springframework.boot") version "3.3.5"
+    kotlin("jvm") version "2.1.20"
+    kotlin("plugin.spring") version "2.1.20"
+    id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.6"
 }
 
