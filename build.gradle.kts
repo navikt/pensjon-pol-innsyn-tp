e281f5ff94fa33a9a04b7299ea00563b8193da26
TheMather1 <the.mather1@gmail.com>
2025-05-13 13:30:36 +0200

    Bump versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 group = "no.nav.pensjon"
 version = "1"
 
-val jacksonVersion = "2.17.2"
+val jacksonVersion = "2.19.0"
 
 plugins {
-    kotlin("jvm") version "2.1.20"
-    kotlin("plugin.spring") version "2.1.20"
-    id("org.springframework.boot") version "3.4.4"
-    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "2.1.21"
+    kotlin("plugin.spring") version "2.1.21"
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 repositories {
@@ -20,8 +20,8 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310", jacksonVersion)
     implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", jacksonVersion)
-    implementation("io.micrometer", "micrometer-registry-prometheus", "1.13.2")
-    implementation("net.logstash.logback", "logstash-logback-encoder", "8.0")
+    implementation("io.micrometer", "micrometer-registry-prometheus", "1.15.0")
+    implementation("net.logstash.logback", "logstash-logback-encoder", "8.1")
     implementation("no.nav.common", "token-client", "2.2023.01.10_13.49-81ddc732df3a")
     implementation("org.apache.poi", "poi-ooxml", "5.3.0")
     implementation("org.springframework.boot", "spring-boot-starter-web")
@@ -32,7 +32,7 @@ dependencies {
     implementation("org.springframework.boot", "spring-boot-starter-thymeleaf")
     testImplementation(kotlin("test-junit5"))
     testRuntimeOnly("org.junit.platform", "junit-platform-launcher")
-    testImplementation("org.wiremock", "wiremock-jetty12", "3.9.1")
+    testImplementation("org.wiremock", "wiremock-jetty12", "3.13.0")
     testImplementation("com.ninja-squad", "springmockk", "4.0.2")
     testImplementation("org.springframework.boot", "spring-boot-starter-test") {
         exclude(module = "mockito-core")
